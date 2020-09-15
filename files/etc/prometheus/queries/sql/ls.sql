5d651dc22becb59c2c8c0f5201054aa1dbadc3c9
ioannis1 <itambouras@users.noreply.github.com>
2020-09-15 17:27:53 -0400

    inc

--- files/etc/prometheus/queries/sql/ls.sql
+++ files/etc/prometheus/queries/sql/ls.sql
@@ -1,8 +1,9 @@
-DROP SCHEMA IF EXISTS pgexporter cASCADE;
+DROP SCHEMA IF EXISTS pgexporter CASCADE;
 CREATE SCHEMA IF NOT EXISTS pgexporter AUTHORIZATION prometheus;
 
 SET SEARCH_PATH  to pgexporter;
 
+GRANT EXECUTE ON FUNCTION ls_dir(text) TO prometheus;
 CREATE OR REPLACE FUNCTION ls_dir( dir text ) RETURNS SETOF text AS
 $$ SELECT * FROM pg_catalog.pg_ls_dir(dir); $$
 LANGUAGE sql
@@ -12,13 +13,14 @@ SECURITY DEFINER;
 GRANT EXECUTE ON FUNCTION ls_dir(text) TO prometheus;
 
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE FUNCTION ls_logdir( ) RETURNS numeric AS
 $$ SELECT sum( (i).size)  FROM pg_catalog.pg_ls_logdir() as i; $$
 LANGUAGE sql
 VOLATILE
 SECURITY DEFINER;
 
-GRANT EXECUTE ON FUNCTION ls_dir(text) TO prometheus;
+-- not needed for pg > REL_11
 GRANT EXECUTE ON FUNCTION ls_logdir()  TO prometheus;
 
 
--- files/etc/prometheus/queries/sql/pg_stat_statements.sql
+++ files/etc/prometheus/queries/sql/pg_stat_statements.sql
@@ -4,34 +4,42 @@ GRANT USAGE ON SCHEMA pgexporter TO prometheus;
 
 SET SEARCH_PATH TO pgexporter ;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE FUNCTION get_pg_stat_activity() RETURNS SETOF pg_stat_activity AS
 $$ SELECT * FROM pg_catalog.pg_stat_activity; $$
 LANGUAGE sql
 VOLATILE
 SECURITY DEFINER;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE VIEW pg_stat_activity AS
   SELECT * from pgexporter.get_pg_stat_activity();
 
+-- not needed for pg > REL_11
 GRANT SELECT ON pg_stat_activity TO prometheus;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE FUNCTION get_pg_stat_replication() RETURNS SETOF pg_stat_replication AS
 $$ SELECT * FROM pg_catalog.pg_stat_replication; $$
 LANGUAGE sql
 VOLATILE
 SECURITY DEFINER;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE VIEW pg_stat_replication AS
   SELECT * FROM pgexporter.get_pg_stat_replication();
 
+-- not needed for pg > REL_11
 GRANT SELECT ON pg_stat_replication TO prometheus;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE FUNCTION get_pg_stat_statements() RETURNS SETOF public.pg_stat_statements AS
 $$ SELECT * FROM public.pg_stat_statements; $$
 LANGUAGE sql
 VOLATILE
 SECURITY DEFINER;
 
+-- not needed for pg > REL_11
 CREATE OR REPLACE VIEW pg_stat_statements AS
   SELECT * FROM pgexporter.get_pg_stat_statements();
 
